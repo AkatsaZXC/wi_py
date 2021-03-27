5ea49e38a0c2e457dccf0d80c57adbb8a49bcc87
AkatsaZXC <akatsaaaaa@gmail.com>
2021-03-27 17:19:12 +0300

    added beautiful prints and gitignore

---
+++ wipy_main.py
@@ -0,0 +1,13 @@
+import os
+import colorama
+
+print(colorama.Fore.YELLOW,f"""_________________________________
+
+ Hello,  {os.getlogin()}!
+ Welcome to Wi-Py!""")
+print(""" ________________________________
+
+ [*]  Created by Akatsa
+ [*]  Open-source project
+ [*]  GitHub: https://github.com/AkatsaZXC 
+ ________________________________""")
