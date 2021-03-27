7a4ae7e4181936c034071d033b42f222939f2c2c
AkatsaZXC <akatsaaaaa@gmail.com>
2021-03-27 18:05:24 +0300

    added run with sudo

--- wipy_main.py
+++ wipy_main.py
@@ -3,6 +3,7 @@ import colorama
 import port_scanner
 
 def main():
+    #  Basic user interface
     print(colorama.Fore.YELLOW,f"""_________________________________
 
     Hello,  {os.getlogin()}!
@@ -13,6 +14,11 @@ def main():
     [*]  Open-source project
     [*]  GitHub: https://github.com/AkatsaZXC 
  ________________________________""")
+#  Check if user run this app with sudo
+if not 'SUDO_UID' in os.environ.keys():
+    print("Run this script with sudo.\n")
+    print("Exiting...")
+    exit()
 
 if __name__ == '__main__':
     main()
